4dfb42f4df22502c272bb762a21171fdcade878a
neakoh <nickymain1213@aol.com>
2025-04-27 14:29:55 +0000

    Update formula to

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,7 +1,7 @@
 class Hello < Formula
   homepage "https://github.com/neakoh/brew-test"
-  url "https://github.com/neakoh/brew-test/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "bf0bb53a7e7db180a44c43a2db1912176c4952a22af0960b052bd74555a4111b"
+  url "https://github.com/neakoh/brew-test/archive/refs/tags/v1.0.6.tar.gz"
+  sha256 "ea84b1d0c81d4cab96157a5b3f15bb382979d2407dd01f883fdb31a3f55010f7"
 
   def install
     bin.install "test.sh" => "hello"
